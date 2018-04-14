aaab77e25160a28b95968c0b0e1b18c0bfa80c4d
Ольга Петрова <olga_petrova@MacBook-Air-Petrova.local>
2018-04-15 03:51:32 +0500

    .

--- Reservation/Controllers/HomeController.cs
+++ Reservation/Controllers/HomeController.cs
@@ -12,7 +12,13 @@ namespace Reservation.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var rooms = new List<MeetingRoom>();
+            rooms.Add(new MeetingRoom(1, "Room 1", 10));
+            rooms.Add(new MeetingRoom(2, "Room 2", 10));
+            rooms.Add(new MeetingRoom(3, "Room 3", 13));
+            rooms.Add(new MeetingRoom(4, "Room 4", 10));
+            rooms.Add(new MeetingRoom(5, "Room 5", 11));
+            return View(rooms);
         }
 
         public IActionResult About()
@@ -26,6 +32,11 @@ namespace Reservation.Controllers
         {
             ViewData["Message"] = "Your contact page.";
 
+            return View();
+        }
+
+        public IActionResult LogIn()
+        {
             return View();
         }
     }
--- Reservation/Models/MeetingRoom.cs
+++ Reservation/Models/MeetingRoom.cs
@@ -3,12 +3,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Reservation.Models
 {
-    public class MeetingRoom                // уж сорян, но так переводчик сказал
+    public class MeetingRoom
     {
         [Required]
         public int Id { get; set; } 
         public string Name { get; set; }
         [Range(1, 60)]                      // просто так сделала ограничение на вместимость, по сути этого можно и не делать
         public int Capacity { get; set; }   // вместимость
+
+        public MeetingRoom (int id, string name, int capacity) // временно (для меня)
+        {
+            Id = id;
+            Name = name;
+            Capacity = capacity;
+        }
     }
 }
