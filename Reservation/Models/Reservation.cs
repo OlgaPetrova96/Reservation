69f83a28b5b5253ded507d3d2b649cfa24270609
Ольга Петрова <olga_petrova@MacBook-Air-Petrova.local>
2018-05-20 13:26:53 +0500

    новые таблички :)

--- Reservation/Models/Reservation.cs
+++ Reservation/Models/Reservation.cs
@@ -5,12 +5,19 @@ namespace Reservation.Models
 {
     public class Reservation
     {
+        [Display(Name = "Номер")] 
         public int Id { get; set; }
-        public int RoomId{ get; set; }
+        [Display(Name = "Комната")] 
+        public int RoomId{ get; set; } // вот тут Id на название переговорки поменяем
+        [Display(Name = "Начало встречи")]
         public DateTime BeginTime { get; set; }
+        [Display(Name = "Окончание встречи")]
         public DateTime EndTime { get; set; }
+        [Display(Name = "Приоритет")]
         public int Priority { get; set; }
+        [Display(Name = "Бронировщик")]
         public string Booker { get; set; }
+        [Display(Name = "Описание")]
         public string Description { get; set; }
       
     }
