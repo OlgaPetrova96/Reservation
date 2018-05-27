591dad5426f42719efefa34fef779cb7344918ef
Ольга Петрова <olga_petrova@MacBook-Air-Petrova.local>
2018-05-28 04:04:18 +0500

    я всё, больше не могу

---
+++ Reservation/Infrastructure/CustomDateTimeModelBinder.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Threading.Tasks;
+ 
+namespace CustomModelBinderApp.Infrastructure
+{
+    public class CustomDateTimeModelBinder : IModelBinder
+    {
+        private readonly IModelBinder fallbackBinder;
+        public CustomDateTimeModelBinder(IModelBinder fallbackBinder)
+        {
+            this.fallbackBinder = fallbackBinder;
+        }
+ 
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+ 
+            var datePartValues = bindingContext.ValueProvider.GetValue("Date");
+            var timePartValues = bindingContext.ValueProvider.GetValue("Time");
+ 
+            if (datePartValues == ValueProviderResult.None || timePartValues == ValueProviderResult.None)
+                return fallbackBinder.BindModelAsync(bindingContext);
+ 
+            string date = datePartValues.FirstValue;
+            string time = timePartValues.FirstValue;
+ 
+            DateTime.TryParse(date, out var parsedDateValue);
+            DateTime.TryParse(time, out var parsedTimeValue);
+ 
+            var result = new DateTime(parsedDateValue.Year,
+                            parsedDateValue.Month,
+                            parsedDateValue.Day,
+                            parsedTimeValue.Hour,
+                            parsedTimeValue.Minute,
+                            parsedTimeValue.Second);
+                             
+            bindingContext.Result = ModelBindingResult.Success(result);
+            return Task.CompletedTask;
+        }
+    }
+}
